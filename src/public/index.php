2478f1871c40950dce7b77b9e84446f594170f3d
Karam Nassar <karam.nassar@hotmail.com>
2024-01-27 10:11:47 +0300

    Add Router to support get and post

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo 'Hello World!';
+declare(strict_types=1);
+
+use App\Core\Router;
+use App\Exceptions\RouteNotFoundException;
+use App\Http\Controllers\InvoiceController;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$router = new Router();
+
+$router->get('/', function () {
+    echo 'Home';
+});
+
+$router->get('/invoice', [InvoiceController::class, 'create']);
+$router->post('/invoice', [InvoiceController::class, 'store']);
+
+try {
+    $router->resolve($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+} catch (RouteNotFoundException $e) {
+    echo $e->getMessage();
+}
